42504cf2bbb37a09f8bec72e536e12d6086690c6
markheger <mark-oliver.heger@de.ibm.com>
2020-01-07 13:10:17 +0000

    resolves #43

--- tests/fwtests/tests/AllSuites/Runtime/EmptyIndex/TestCase.sh
+++ tests/fwtests/tests/AllSuites/Runtime/EmptyIndex/TestCase.sh
@@ -8,7 +8,7 @@ STEPS='es_runStandalone Evaluate'
 
 Evaluate() {
 	# check for the error on empty index attr
-	if ! egrep "CDIST3507E Unknown index. The specified index string is either 'null' or empty. Input tuple is ignored." standalone.log
+	if ! egrep "CDIST3507E Unknown index" standalone.log
 	then
 		setFailure "Pattern not found in log" 
 	fi
